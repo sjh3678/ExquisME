393173c38b3f6b37944e0ec25c63d0b91597809e
SonHyunBin <a01047290225@gmail.com>
2021-11-08 17:10:18 +0900

    로그인 컨트롤러 초기화

--- src/main/java/web/controller/LoginController.java
+++ src/main/java/web/controller/LoginController.java
@@ -1,16 +1,5 @@
 package web.controller;
 
 public class LoginController {
-	public static void main(String[] args) {
-//		System.out.println("111");
-//		System.out.println("재현");
-//		System.out.println("55555555555");
-//		System.out.println("현빈");
-//		System.out.println("재현찐막가자 제발!!");
-		System.out.println("창무");
-		System.out.println("다들 주말 잘 보내세요~");
-		System.out.println("클래스다이어그램");
-		System.out.println("창무창무");
-		
-	}
+	
 }
